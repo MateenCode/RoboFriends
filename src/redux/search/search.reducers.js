6675314203f031a5905e76797418937819dcd5c6
Mateen Kazia <mateencode@gmail.com>
2020-04-22 14:14:06 -0500

    action completed

---
+++ src/redux/search/search.reducers.js
@@ -0,0 +1,12 @@
+import { CHANGE_SEARCH_FIELD } from "./search.types";
+
+const intialState = {
+  searchField: "",
+};
+
+export const searchRobots = (state = intialState, action = {}) => {
+    switch(action.type){
+        case: CHANGE_SEARCH_FIELD: 
+        return 
+    }
+};
