6d63bc3d177d73fd78cd2fc86fe8665d338d5fbd
Mateen Kazia <mateenkazia@Mateens-MacBook-Pro.local>
2020-04-27 12:39:08 -0500

    robot reducer added

--- src/redux/robot/robot.reducers.js
+++ src/redux/robot/robot.reducers.js
@@ -20,6 +20,13 @@ export const searchRobots = (state = intialState, action = {}) => {
 
 export const requestRobots = (state = intialState, action = {}) => {
   switch (action.type) {
-    case REQUEST_ROBOTS_PENDING
+    case REQUEST_ROBOTS_PENDING:
+      return { ...state, isPending: true };
+    case REQUEST_ROBOTS_SUCCESS:
+      return { ...state, robots: action.payload, isPending: false };
+    case REQUEST_ROBOTS_FAILED:
+      return { ...state, error: action.payload, isPending: false };
   }
+  default:
+    return state
 };
